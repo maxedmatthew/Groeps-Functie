edc6a192facd2ff6f9dc8008030bc6b4ad382df6
Unknown <matthewyt1998@gmail.com>
2018-01-06 14:16:14 +0100

    Random Number Generator
    De ID's zijn nu random generated

--- js/groepsfunctie.js
+++ js/groepsfunctie.js
@@ -6,48 +6,75 @@ var Tabel = document.createElement("TABLE");
      document.body.appendChild(Tabel);
 
 var tabelrij = document.createElement("tr");
-    Tabel.appendChild(tabelrij);
+Tabel.appendChild(tabelrij);
+
+
+    
 
 // Tabel kolom namen aanmaken
-var addKolomNaam = ( function(){
+function KolomnaamToevoeg(kolomaantal ,kn1, kn2, kn3, kn4, kn5, kn6){
+   
     
-    for (var i = 0; i < 4; i++){
+    for (var i = 0; i < kolomaantal; i++){
 
         var tdHead = document.createElement("th");
         tabelrij.appendChild(tdHead);
         tdHead.innerHTML = "test";
+        
     
         if(i == 0){
-            
-             tdHead.innerHTML = "Naam";
+        
+            tdHead.innerHTML = kn1;
+        
         } else if(i == 1){
-        
-            tdHead.innerHTML = "Status";
+            
+                tdHead.innerHTML = kn2;
+            
         } else if(i == 2){
-        
-             tdHead.innerHTML = "Permissie";
+    
+                tdHead.innerHTML = kn3;
+            
         } else if(i == 3){
-        
-            tdHead.innerHTML = "Verander Permissie";
+            
+                tdHead.innerHTML = kn4;            
         }
+            else if(i == 4){
+                
+                if (kn5 == undefined){
+                    tdHead.innerHTML = "Extra Kolom";
+                }
+                else{
+                    tdHead.innerHTML = kn5;
+                }
+        }
+           else if(i == 5){
+
+                if (kn6 == undefined){
+                    tdHead.innerHTML = "Extra Kolom";
+                }
+                else{
+                    tdHead.innerHTML = kn6;
+                }
+        }
+      
    
     }
 
-})();
-  
-   
+}
     
+  
 
 // Tabel invullen met waardes
-var addTabel = ( function(){
+function addTabel(n){
+    
     
     for (var i = 0; i < aantalPersonen; i++){
     
-         var ChangePermissieBtn = document.createElement('BUTTON');
-        ChangePermissieBtn.innerText = 'Klik op mij';
-        ChangePermissieBtn.onclick = function testFunctie(){
-        
-        }
+        var ChangePermissieBtn = document.createElement('BUTTON');
+        ChangePermissieBtn.innerText = 'Verwijder';
+        ChangePermissieBtn.onclick = function testfunctie(){
+            console.log("test");
+        };
    
   
         // Alle rijen worden aangemaakt, aantal rijen ligt aan lengte van array.
@@ -59,13 +86,19 @@ var addTabel = ( function(){
         var tdStatus = document.createElement("td");
         var tdPermissie = document.createElement("td");
         var tdChangePermissie = document.createElement("td");
-    
+        
+        
         // Alle kolommen worden toegevoegd aan HTML
         tabelrij.appendChild(tdNaam);
         tabelrij.appendChild(tdStatus);
         tabelrij.appendChild(tdPermissie);
         tabelrij.appendChild(tdChangePermissie);
-    
+        
+        if (n >= 5){
+        var tdID = document.createElement("td");
+        tabelrij.appendChild(tdID);
+        tdID.innerHTML = personen[i].code;
+        }
     
     
         // Alle kolommen worden gevuld met de juiste informatie
@@ -77,8 +110,21 @@ var addTabel = ( function(){
     
     }
     
-})();
+}
     
+
+function makeid(){
+  
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+
+    for (var i = 0; i < 5; i++)
+    text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+  return text;
+    
+}
+  
     
 function addPersoontoDB(){
     
@@ -86,7 +132,7 @@ function addPersoontoDB(){
     lastname = document.getElementById("newLastname").value;
     permissie = document.getElementById("newPermissie").value;
     
-    var nieuwpersoon = {vNaam:name, aNaam:lastname, Status:"Offline", Permissie:permissie};
+    var nieuwpersoon = {vNaam:name, aNaam:lastname, Status:"Offline", Permissie:permissie, test:"1E2MS"};
     personen.push(nieuwpersoon);
      
     //document.getElementById("nameList").innerHTML = names.toString();   
@@ -94,15 +140,16 @@ function addPersoontoDB(){
     
 
     
-    (function addNewPerson(){
+    function addNewPerson(n){
         
-         var ChangePermissieBtn = document.createElement('BUTTON');
-        ChangePermissieBtn.innerText = 'Klik op mij';
+        var ChangePermissieBtn = document.createElement('BUTTON');
+        ChangePermissieBtn.innerText = 'Verwijder';
         ChangePermissieBtn.onclick = function testfunctie(){
-            
-            nieuwpersoon.Permissie == "test";
+            console.log("test");
         };
-    
+   
+  
+        // Alle rijen worden aangemaakt, aantal rijen ligt aan lengte van array.
         var tabelrij = document.createElement("tr");
         Tabel.appendChild(tabelrij);
     
@@ -111,12 +158,19 @@ function addPersoontoDB(){
         var tdStatus = document.createElement("td");
         var tdPermissie = document.createElement("td");
         var tdChangePermissie = document.createElement("td");
-        
+      
+    
+        // Alle kolommen worden toegevoegd aan HTML
         tabelrij.appendChild(tdNaam);
         tabelrij.appendChild(tdStatus);
         tabelrij.appendChild(tdPermissie);
         tabelrij.appendChild(tdChangePermissie);
-     
+        
+        if (n >= 5){
+        var tdID = document.createElement("td");
+        tabelrij.appendChild(tdID);
+        tdID.innerHTML = (makeid());
+            }
         
         tdNaam.innerHTML = name;
         tdStatus.innerHTML = lastname;
@@ -124,15 +178,22 @@ function addPersoontoDB(){
         tdChangePermissie.appendChild(ChangePermissieBtn);
         
         
-    })();
+    }
+    
+    new addNewPerson(grootte);
     
    
 
 } 
+    
+var grootte = 5;
+new KolomnaamToevoeg(grootte,'Naam','Status','Permissie','Delete', 'ID','Reginald');
+new addTabel(grootte);
 
     return {
         
-        addPersoontoDB: addPersoontoDB
+        addPersoontoDB: addPersoontoDB,
+        KolomnaamToevoeg: KolomnaamToevoeg
         
     }
 
--- js/personenDB.js
+++ js/personenDB.js
@@ -2,44 +2,26 @@
 {
     vNaam : "Erik",
     aNaam  : "van Kampen",
-    Status : "Online",
-    Permissie  : "wel"
+    Status : "ADMIN",
+    Permissie  : "wel",
+    code : "21E4D"
 },    //personen aanmaken voor in de libraries
 {
     vNaam : "Willem",
     aNaam  : "van Durnen",
     Status : "Online",
-    Permissie  : "geen"
+    Permissie  : "geen",
+    code : "HC4D1"
 },     //personen aanmaken voor in de libraries
     
 { 
     vNaam : "Stefan",
     aNaam  : "de Vrij",
     Status : "Offline",
-    Permissie  : "wel"
+    Permissie  : "wel",
+    code : "NS4K2"
 }
 ]; 
  
 
 
-// nog niet toegevoegd persoon
-var extrapersonen = [
-    { 
-    vNaam : "Dominique",
-    aNaam  : "Brouwers",
-    Status : "Online",
-    Permissie  : "niet"
-    },
-    { 
-    vNaam : "Kevin",
-    aNaam  : "van Huizen",
-    Status : "Offline",
-    Permissie  : "niet"
-    },
-    { 
-    vNaam : "Pelle",
-    aNaam  : "Vissers",
-    Status : "Offline",
-    Permissie  : "niet"
-    }
-];
